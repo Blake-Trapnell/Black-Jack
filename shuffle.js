73ac8f4c0b90777a3a22bee4bf42123310088de4
Blake Trapnell <blake.trapnell103@gmail.com>
2021-02-02 21:54:53 -0700

    added cut to shuffle

--- shuffle.js
+++ shuffle.js
@@ -1,4 +1,5 @@
-let deck = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52];
+let deck = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52]
+const half = deck.length / 2;
 
 const wash = () => {
 	for (let i = 0; i < 50; i++) {
@@ -12,7 +13,6 @@ const wash = () => {
 };
 
 const riffle = () => {
-	const half = deck.length / 2;
 	const firstHalf = deck.splice(0, half);
 	const secondHalf = deck.splice(-half);
 	const riffledDeck = [];
@@ -37,10 +37,18 @@ const box = () => {
 	deck = boxedDeck.concat(deck);
 };
 
+const cut = () => {
+	let cutDeck = deck.splice(-half);
+	cutDeck = cutDeck.concat(deck);
+	deck = cutDeck;
+};
+
 const shuffle = () => {
 	wash();
 	riffle();
 	box();
+	riffle();
+	cut();
 };
 
 console.log(deck);
