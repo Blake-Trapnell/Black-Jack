fa6f1b8a02269bff272ae5f6d517d76eae68df46
Blake Trapnell <blake.trapnell103@gmail.com>
2021-02-03 19:53:34 -0700

    installed scss, added table background with texture

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import Table from "./components/table/table.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Table/>
     </div>
   );
 }
---
+++ src/components/table/table.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./table.scss";
+
+const Table = () => {
+    return (
+        <div className={"poker-table"}> 
+        </div>
+    )
+}
+
+export default Table;
